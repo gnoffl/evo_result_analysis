450884e3e97f8f9ca96dc854e0401802b4e73940
Gernot Schmitz <spammail1240@web.de>
2025-10-13 17:27:36 +0200

    printing for debugging

--- src/analysis/candidate_selection.py
+++ src/analysis/candidate_selection.py
@@ -32,6 +32,7 @@ def select_candidates(results_folder: str, output_path: str, starting_fitness_ma
     selected = {}
     for pareto_path in pareto_paths:
         selected.setdefault("gene", []).append(os.path.basename(os.path.dirname(os.path.dirname(pareto_path))))
+        print(selected["gene"][-1])
         with open(pareto_path, 'r') as file:
             pareto_front = json.load(file)
         start_fitness = pareto_front[-1][1]
@@ -46,7 +47,7 @@ def select_candidates(results_folder: str, output_path: str, starting_fitness_ma
                 value = get_min_mutation_count_for_fitness(pareto_front, fitness)
             selected.setdefault(f"mutations_for_fitness_{fitness:.1f}", []).append(value)
             selected.setdefault(f"sequence_for_fitness_{fitness:.1f}", []).append(
-                get_sequence_at_mutation_count(pareto_front, int(value)) if not np.isnan(value) else ""
+                get_sequence_at_mutation_count(pareto_front, value) if not np.isnan(value) else ""
             )
         selected.setdefault("reference_sequence", []).append(pareto_front[-1][0])
         selected.setdefault("max_num_mutations", []).append(max_num_mutations)
