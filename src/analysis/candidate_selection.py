c86872422bc84b43d96cd8552a85460915e79a63
Gernot Schmitz <spammail1240@web.de>
2025-10-14 16:23:29 +0200

    added functionality to select candidate sequences.

--- src/analysis/candidate_selection.py
+++ src/analysis/candidate_selection.py
@@ -24,7 +24,7 @@ def get_data_at_mutation_count(pareto_front: List[Tuple[str, float, float]], tar
         raise ValueError(f"Mutation count {target_mutation_count} not found in pareto front.")
 
 
-def select_candidates(results_folder: str, output_path: str, starting_fitness_max: Optional[float] = None, starting_fitness_min: Optional[float] = None) -> None:
+def preliminary_selection(results_folder: str, output_path: str, starting_fitness_max: Optional[float] = None, starting_fitness_min: Optional[float] = None) -> None:
     """
     Select genes with the lowest starting values from analysis results.
     """
@@ -84,44 +84,152 @@ def load_selected_genes(csv_path: str) -> pd.DataFrame:
     selected = pd.read_csv(csv_path, index_col="gene")
     return selected
 
+def draw_line_plot(vis_folder, gene, single_points, multi_points):
+    single_mutations = [point[1] for point in single_points]
+    multi_mutations = [point[1] for point in multi_points]
+    output_path = os.path.join(vis_folder, f"{gene}_trajectory_comparison.png")
+    plt.figure()
+    plt.plot([point[0] for point in single_points], single_mutations, marker='o', label='Single Mutation')
+    plt.plot([point[0] for point in multi_points], multi_mutations, marker='o', label='Multi Mutation')
+    plt.xlabel('Fitness')
+    plt.ylabel('Number of Mutations')
+    plt.xlim(-0.03, 1.03)
+    plt.ylim(-3, 93)
+    plt.title(f'Fitness vs. Number of Mutations for {gene}')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(output_path, bbox_inches='tight')
+    plt.close()
 
-def compare_trajectories_mutations():
-    selected_single = load_selected_genes("/home/gernot/ARCitect/ARCs/genRE/assays/Evolution/protocols/paper/analysis/GOF_LOF/GOF/GOF_single_mutation/candidate_genes_GOF_single.csv")
-    selected_multi = load_selected_genes("/home/gernot/ARCitect/ARCs/genRE/assays/Evolution/protocols/paper/analysis/GOF_LOF/GOF/GOF_multi_mutation/candidate_genes_GOF_multi.csv")
-    merged = selected_single.merge(selected_multi, left_index=True, right_index=True, suffixes=("_single", "_multi"))
+def get_line_plot_data(row):
+    mutation_cols_single = [col for col in row.index if col.startswith("mutations_for_fitness_") and col.endswith("_single")]
+    mutation_cols_multi = [col for col in row.index if col.startswith("mutations_for_fitness_") and col.endswith("_multi")]
+    single_points = []
+    multi_points = []
+    for col in mutation_cols_single:
+        single_points.append((float(col.split("_")[-2]), row[col]))
+    for col in mutation_cols_multi:
+        multi_points.append((float(col.split("_")[-2]), row[col]))
+    single_points.append((row["start_fitness_single"], 0))
+    single_points.append((row["final_fitness_single"], row["max_num_mutations_single"]))
+    multi_points.append((row["start_fitness_multi"], 0))
+    multi_points.append((row["final_fitness_multi"], row["max_num_mutations_multi"]))
+    single_points = sorted(single_points, key=lambda x: x[0])
+    multi_points = sorted(multi_points, key=lambda x: x[0])
+    return single_points,multi_points
+
+
+def compare_trajectories_mutations(merged: pd.DataFrame):
     vis_folder = os.path.join("visualization", "GOF_LOF", "GOF", "compare_trajectories")
     os.makedirs(vis_folder, exist_ok=True)
     for gene , row in merged.iterrows():
-        mutation_cols_single = [col for col in merged.columns if col.startswith("mutations_for_fitness_") and col.endswith("_single")]
-        mutation_cols_multi = [col for col in merged.columns if col.startswith("mutations_for_fitness_") and col.endswith("_multi")]
-        single_points = []
-        multi_points = []
-        for col in mutation_cols_single:
-            single_points.append((float(col.split("_")[-2]), row[col]))
-        for col in mutation_cols_multi:
-            multi_points.append((float(col.split("_")[-2]), row[col]))
-        single_points.append((row["start_fitness_single"], 0))
-        single_points.append((row["final_fitness_single"], row["max_num_mutations_single"]))
-        multi_points.append((row["start_fitness_multi"], 0))
-        multi_points.append((row["final_fitness_multi"], row["max_num_mutations_multi"]))
-        single_points = sorted(single_points, key=lambda x: x[0])
-        multi_points = sorted(multi_points, key=lambda x: x[0])
-        single_mutations = [point[1] for point in single_points]
-        multi_mutations = [point[1] for point in multi_points]
-        output_path = os.path.join(vis_folder, f"{gene}_trajectory_comparison.png")
-        plt.figure()
-        plt.plot([point[0] for point in single_points], single_mutations, marker='o', label='Single Mutation')
-        plt.plot([point[0] for point in multi_points], multi_mutations, marker='o', label='Multi Mutation')
-        plt.xlabel('Fitness')
-        plt.ylabel('Number of Mutations')
-        plt.xlim(-0.03, 1.03)
-        plt.ylim(-3, 93)
-        plt.title(f'Fitness vs. Number of Mutations for {gene}')
-        plt.legend()
-        plt.grid(True)
-        plt.savefig(output_path, bbox_inches='tight')
-        plt.close()
+        single_points, multi_points = get_line_plot_data(row)
+        draw_line_plot(vis_folder, gene, single_points, multi_points)
 
+def get_best_fitness_and_seq(row, mutations, single_col, multi_col) -> Tuple[float, str]:
+    if pd.isna(row[single_col]) and pd.isna(row[multi_col]):
+        return np.nan, ""
+    elif pd.isna(row[single_col]):
+        return row[multi_col], row[f"sequence_for_{mutations}_mutations_multi"]
+    elif pd.isna(row[multi_col]):
+        return row[single_col], row[f"sequence_for_{mutations}_mutations_single"]
+    else:
+        if row[single_col] > row[multi_col]:
+            return row[single_col], row[f"sequence_for_{mutations}_mutations_single"]
+        else:
+            return row[multi_col], row[f"sequence_for_{mutations}_mutations_multi"]
+
+def find_best_mutation_deltas(merged: pd.DataFrame):
+    for mutations in [1, 2, 3, 5, 7, 10, 15, 20]:
+        best_fitnesses, best_sequences = [], []
+        single_col = f"fitness_delta_for_{mutations}_mutations_single"
+        multi_col = f"fitness_delta_for_{mutations}_mutations_multi"
+        for gene, row in merged.iterrows():
+            best_fitness, best_sequence = get_best_fitness_and_seq(row=row, mutations=mutations, single_col=single_col, multi_col=multi_col)
+            best_fitnesses.append(best_fitness)
+            best_sequences.append(best_sequence)
+        merged[f"best_fitness_delta_for_{mutations}_mutations"] = best_fitnesses
+        merged[f"per_mutation_delta_for_{mutations}_mutations"] = [val/mutations if not pd.isna(val) else np.nan for val in best_fitnesses]
+        merged[f"best_sequence_for_{mutations}_mutations"] = best_sequences
+
+
+def find_best_mutation_count_per_gene(row: pd.Series, rank: int = 1) -> int:
+    per_mutation_columns = [col for col in row.index if col.startswith("per_mutation_delta_for_") and col.endswith("_mutations")]
+    # get column name with the highest value
+    for _ in range(rank):
+        best_col = max(per_mutation_columns, key=lambda col: row[col] if pd.notna(row[col]) else -np.inf) #type:ignore
+        per_mutation_columns.remove(best_col)
+    return best_col.split("_")[-2]
+
+def find_best_mutations_per_gene(merged: pd.DataFrame, n_candidates: int):
+    for results_rank in range(1, n_candidates + 1):
+        merged[f"best_mutation_count_rank_{results_rank}"] = merged.apply(lambda row: find_best_mutation_count_per_gene(row, rank=results_rank), axis=1)
+
+def selected_per_mutation(merged: pd.DataFrame, n_candidates: int) -> pd.DataFrame:
+    mutations = [1, 2, 3, 5, 7, 10, 15, 20]
+    best_deltas, best_delta_genes, best_delta_sequences, references, mutation_col, start_fitnesses, fitnesses = [], [], [], [], [], [], []
+    rel_cols = [col for col in merged.columns if col.startswith("best_fitness_delta_for_") or col.startswith("best_sequence_for_") or (col.startswith("sequence_for_") and col.endswith("_mutations")) or col.startswith("reference")]
+    relevant_merged = merged[rel_cols]
+    best_delta_cols = [f"best_fitness_delta_for_{m}_mutations" for m in mutations]
+    for col in best_delta_cols:
+        curr_df = relevant_merged.sort_values(by=col, ascending=False).iloc[:n_candidates]
+        for gene in curr_df.index:
+            mutation_col.append(col.split("_")[-2])
+            best_deltas.append(curr_df.loc[gene, col])
+            best_delta_genes.append(gene)
+            best_delta_sequences.append(curr_df.loc[gene, col.replace("best_fitness_delta", "best_sequence")])
+            references.append(curr_df.loc[gene, "reference_sequence_single"])
+            start_fitnesses.append(merged.loc[gene, "start_fitness_single"])
+            fitnesses.append(start_fitnesses[-1] + best_deltas[-1])
+    summary = pd.DataFrame({
+        "gene": best_delta_genes,
+        "mutations": mutation_col,
+        "start_fitness": start_fitnesses,
+        "best_fitness_delta": best_deltas,
+        "fitness": fitnesses,
+        "best_sequence": best_delta_sequences,
+        "reference_sequence": references,
+    })
+    summary["selection_reason"] = "top_per_mutation"
+    return summary
+
+
+def selected_per_gene(merged: pd.DataFrame, n_candidates: int) -> pd.DataFrame:
+    best_deltas, best_delta_genes, best_delta_sequences, references, mutation_col, start_fitnesses, fitnesses = [], [], [], [], [], [], []
+    for gene, row in merged.iterrows():
+        for mutation_col_rank in [f"best_mutation_count_rank_{i}" for i in range(1, n_candidates + 1)]:
+            best_mutation_count = int(row[mutation_col_rank])
+            best_deltas.append(row[f"best_fitness_delta_for_{best_mutation_count}_mutations"])
+            best_delta_genes.append(gene)
+            best_delta_sequences.append(row[f"best_sequence_for_{best_mutation_count}_mutations"])
+            references.append(row["reference_sequence_single"])
+            start_fitnesses.append(row["start_fitness_single"])
+            fitnesses.append(start_fitnesses[-1] + best_deltas[-1])
+            mutation_col.append(best_mutation_count)
+    summary = pd.DataFrame({
+        "gene": best_delta_genes,
+        "mutations": mutation_col,
+        "start_fitness": start_fitnesses,
+        "best_fitness_delta": best_deltas,
+        "fitness": fitnesses,
+        "best_sequence": best_delta_sequences,
+        "reference_sequence": references,
+    })
+    summary["selection_reason"] = "top_per_gene"
+    return summary
+
+
+def final_selection(single_path: str, multi_path: str, output_path: str, n_candidates: int):
+    selected_single = load_selected_genes(single_path)
+    selected_multi = load_selected_genes(multi_path)
+    merged = selected_single.merge(selected_multi, left_index=True, right_index=True, suffixes=("_single", "_multi"))
+    find_best_mutation_deltas(merged)
+    find_best_mutations_per_gene(merged, n_candidates=n_candidates)
+    per_gene_bests = selected_per_gene(merged, n_candidates=n_candidates)
+    per_gene_bests = per_gene_bests[per_gene_bests["best_fitness_delta"] > 0.3]
+    per_mutation_bests = selected_per_mutation(merged, n_candidates=n_candidates)
+    final_selection = pd.concat([per_gene_bests, per_mutation_bests]).drop_duplicates(subset=["gene", "mutations"])
+    final_selection.to_csv(output_path, index=False)
 
 
 def one_off_error_correction(path):
@@ -137,12 +245,30 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Select genes with the lowest starting values from analysis results.")
     parser.add_argument("--results_folder", "-r", type=str, help="Path to the folder containing analysis results.")
     parser.add_argument("--output_path", "-o", type=str, help="Path to save the selected genes CSV file.")
+    parser.add_argument("--n_candidates", "-n", type=int, default=3, help="Number of candidate genes to select per mutation count and per gene.")
+    parser.add_argument("--single_path", "-s", type=str, help="Path to the CSV file with single mutation results.")
+    parser.add_argument("--multi_path", "-m", type=str, help="Path to the CSV file with multi mutation results.")
     parser.add_argument("--starting_fitness_max", "-max", type=float, default=None, help="Maximum starting fitness to filter genes.")
     parser.add_argument("--starting_fitness_min", "-min", type=float, default=None, help="Minimum starting fitness to filter genes.")
+    parser.add_argument("--final_selection", "-f", action="store_true", help="Perform final selection step.")
+    parser.add_argument("--preliminary_selection", "-p", action="store_true", help="Perform preliminary selection step.")
     return parser.parse_args()
 
 if __name__ == "__main__":
+    # PARAMETERS FOR GOF CANDIDATE SELECTION
+    # final_selection(single_path="/home/gernot/ARCitect/ARCs/genRE/assays/Evolution/protocols/paper/analysis/GOF_LOF/GOF/GOF_single_mutation/candidate_genes_GOF_single.csv",
+    #                 multi_path="/home/gernot/ARCitect/ARCs/genRE/assays/Evolution/protocols/paper/analysis/GOF_LOF/GOF/GOF_multi_mutation/candidate_genes_GOF_multi.csv",
+    #                 output_path="selected_genes_GOF.csv",
+    #                 n_candidates=3)
     args = parse_args()
-    select_candidates(args.results_folder, args.output_path, args.starting_fitness_max, args.starting_fitness_min)
-    # compare_trajectories_mutations()
-    # one_off_error_correction("/home/gernot/ARCitect/ARCs/genRE/assays/Evolution/protocols/paper/analysis/GOF_LOF/GOF/GOF_multi_mutation/candidate_genes_GOF_multi.csv")
+    if not args.final_selection and not args.preliminary_selection:
+        raise ValueError("Either --final_selection (-f) or --preliminary_selection (-p) must be specified.")
+    if args.final_selection and args.preliminary_selection:
+        raise ValueError("Only one of --final_selection (-f) or --preliminary_selection (-p) can be specified at a time.")
+    if args.preliminary_selection:
+        preliminary_selection(args.results_folder, args.output_path, args.starting_fitness_max, args.starting_fitness_min)
+    if args.final_selection:
+        final_selection(single_path=args.single_path,
+                        multi_path=args.multi_path,
+                        output_path=args.output_path,
+                        n_candidates=args.n_candidates)
